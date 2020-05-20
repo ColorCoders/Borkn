002f24c049c4bc85b2c1d2862bc3fb6ec3968836
mattcope94 <mattcope94dev@gmail.com>
2020-05-20 13:01:55 -0400

    some routing

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(public router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  //Get method for carousel or scrollable list
+  showDogs(){
+
+  }
+}
